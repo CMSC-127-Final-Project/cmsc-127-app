ac65b701c6c7a69b06d1c84796d5e20f52e11568
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-03-29 00:43:12 +0800

    feat: added navbar

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from '@/components/ui/navbar';
-import WelcomeBanner from './components/welcome';
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
-      <WelcomeBanner username="Celeru00" />
     </>
   );
 }
