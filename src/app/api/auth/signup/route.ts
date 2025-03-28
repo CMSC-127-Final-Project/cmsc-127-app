e4fec134f8c01da41ac6de4dfd5ad3df4c3384b1
Gilbrad <gcalanza1@up.edu.ph>
2025-03-28 16:42:03 +0800

    feat: created api routing for signup auth

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "@/lib/supabase";
+
+export async function POST(request: NextRequest) {
+    try {
+        const formData = await request.json();
+        const { data, error } = await supabase.auth.signUp({ email: formData.email, password: formData.password });
+
+        if (error) throw { message: error.message, status: error.status, name: error.name };   
+        
+        const { error: insertError } = await supabase.from("User").insert({
+                  auth_id: data.user?.id,
+                  name: formData.name,
+                  email: formData.email,
+                  role: formData.role,
+                  dept: formData.department,
+                })
+
+        if (insertError) throw { message: insertError.message, name: insertError.name, status: insertError.code };
+        return NextResponse.json({ message: "User created successfully" }, { status: 200 });
+    } catch (error) {
+        const { message, status, name } = error as { message: string; status?: number; name?: string };
+        return NextResponse.json({ error: message, status, name }, { status: status || 500 });
+    }
+}
