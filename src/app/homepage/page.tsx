684a0f3d1644d9afe27c920bdcd6d9aa8987190c
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-03-29 01:00:19 +0800

    chore: added homepage metadata

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,4 +1,10 @@
 import Navbar from '@/components/ui/navbar';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Homepage of the application',
+};
 
 export default function HomePage() {
   return (
