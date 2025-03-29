038e935102149a662290eb8c115cf4ebe0489acf
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-03-29 11:58:58 +0800

    chore: added favicon

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Home',
+  icons: {
+    icon: '/upfavicon.ico'}, 
   description: 'Homepage of the application',
 };
 
