cba5743312f6948460275c35c85c3998e73261c7
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-04-05 14:52:16 +0800

    fix: eslint issues

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabase';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const { error } = await supabase.auth.signOut();
 
   if (error) {
