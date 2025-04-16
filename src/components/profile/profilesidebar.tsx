41699b8acd4407fc0f36059eaaa71845b45c1217
Cherlie <cipalarpalar@up.edu.ph>
2025-04-16 16:23:41 +0800

    feat: routing of profile sidebar to support

--- src/components/profile/profilesidebar.tsx
+++ src/components/profile/profilesidebar.tsx
@@ -29,7 +29,10 @@ export default function ProfileSidebar() {
               <Settings className="h-4 w-4 mr-2 text-[#6b1d1d]" />
               <span className="text-sm">Settings</span>
             </div>
-            <div className="p-3 hover:bg-gray-50 cursor-pointer flex items-center">
+            <div 
+              className="p-3 hover:bg-gray-50 cursor-pointer flex items-center"
+              onClick={() => router.push('/support')}
+            >
               <HelpCircle className="h-4 w-4 mr-2 text-[#6b1d1d]" />
               <span className="text-sm">Help and Support</span>
             </div>
