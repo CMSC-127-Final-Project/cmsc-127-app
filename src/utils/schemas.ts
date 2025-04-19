dbc7a0d7e9566609fafe3866c9905a98a9b7999a
Cherlie <cipalarpalar@up.edu.ph>
2025-04-19 22:43:35 +0800

    feat: prettier style

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -12,15 +12,15 @@ export const changePasswordSchema = z.object({
 });
 
 export const signupSchema = z.object({
-    email: z.string().email('Invalid email address'),
-    password: z.string().min(8, 'Password must be at least 8 characters long'),
-    fname: z.string().min(1, 'First name is required'),
-    lname: z.string().min(1, 'Last name is required'),
-    nickname: z.string().optional(),
-    role: z.enum(['Student', 'Instructor']),
-    department: z.string().min(1, 'Department is required'),
-    studentNumber: z.string().optional(),
-    instructorID: z.string().optional(),
-    instructorOffice: z.string().optional(),
-    facultyRank: z.string().optional(),
-  });
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
+  fname: z.string().min(1, 'First name is required'),
+  lname: z.string().min(1, 'Last name is required'),
+  nickname: z.string().optional(),
+  role: z.enum(['Student', 'Instructor']),
+  department: z.string().min(1, 'Department is required'),
+  studentNumber: z.string().optional(),
+  instructorID: z.string().optional(),
+  instructorOffice: z.string().optional(),
+  facultyRank: z.string().optional(),
+});
