4542801a91b1dd05f86f4c3787eb0a8a36737c02
Cherlie <cipalarpalar@up.edu.ph>
2025-04-23 03:38:45 +0800

    feat: initial backend for client update

--- src/app/api/user/update/route.ts
+++ src/app/api/user/update/route.ts
@@ -17,7 +17,7 @@ export async function PATCH(req: NextRequest) {
 
     const { data: userRecord, error: userRecordError } = await supabase
       .from('User')
-      .select('user_ID, nickname, phone')
+      .select('*')
       .eq('auth_id', sessionAuthId)
       .single();
 
@@ -25,8 +25,6 @@ export async function PATCH(req: NextRequest) {
       console.error('User record error:', userRecordError?.message || 'User not found');
       return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
-
-    console.log('User ID:', userRecord.user_ID);
 
     const body = await req.json();
     const parsedData = updateUserSchema.safeParse(body);
@@ -39,6 +37,35 @@ export async function PATCH(req: NextRequest) {
     }
 
     const filteredUpdates: Record<string, string> = {};
+
+    if (parsedData.data.email && parsedData.data.email !== userRecord.email) {
+      filteredUpdates.email = parsedData.data.email;
+    }
+    if (parsedData.data.first_name && parsedData.data.first_name !== userRecord.first_name) {
+      filteredUpdates.first_name = parsedData.data.first_name;
+    }
+    if (parsedData.data.last_name && parsedData.data.last_name !== userRecord.last_name) {
+      filteredUpdates.last_name = parsedData.data.last_name;
+    }
+    if (parsedData.data.id_number && parsedData.data.id_number !== userRecord.student_num) {
+      if (parsedData.data.role === 'Instructor') {
+        filteredUpdates.instructor_id = parsedData.data.id_number;
+      } else if (parsedData.data.role === 'Student') {
+        filteredUpdates.student_num = parsedData.data.id_number;
+      }
+      else { 
+        return NextResponse.json({ error: 'Invalid role for ID number' }, { status: 400 });
+      }
+    }
+    if (parsedData.data.dept && parsedData.data.dept !== userRecord.dept) {
+      filteredUpdates.dept = parsedData.data.dept;
+    }
+    if (parsedData.data.rank && parsedData.data.rank !== userRecord.rank) {
+      filteredUpdates.rank = parsedData.data.rank;
+    }
+    if (parsedData.data.instructor_office && parsedData.data.instructor_office !== userRecord.instructor_office) {
+      filteredUpdates.instructor_office = String(parsedData.data.instructor_office);
+    }
     if (parsedData.data.nickname && parsedData.data.nickname !== userRecord.nickname) {
       filteredUpdates.nickname = parsedData.data.nickname;
     }
@@ -55,7 +82,7 @@ export async function PATCH(req: NextRequest) {
     const { error } = await supabase
       .from('User')
       .update(filteredUpdates)
-      .eq('user_ID', userRecord.user_ID);
+      .eq('auth_id', userRecord.auth_id);
 
     if (error) {
       console.error('Database error:', error.message);
