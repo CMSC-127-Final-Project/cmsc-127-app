d92b8803dc4950518a66008a14c4aae975946c3b
Cherlie <cipalarpalar@up.edu.ph>
2025-04-24 13:07:22 +0800

    fix: dropdown bug

--- src/components/admin/users.tsx
+++ src/components/admin/users.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React from 'react';
-import { useState, useEffect, useCallback } from 'react';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Search, List } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { RxDotsHorizontal, RxPencil1, RxTrash, RxLockOpen1 } from 'react-icons/rx';
+import { useToast } from '@/hooks/use-toast';
 import { useRouter } from 'next/navigation';
-import UserActionsDropdown from '@/components/admin/actionsDropdown';
+import { List, Search } from 'lucide-react';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 interface User {
   auth_id: string;
@@ -17,13 +17,58 @@ interface User {
   email: string;
   phone: string;
   role: string;
-  department: string;
+  dept: string;
 }
 
-export default function Users() {
+const DropdownPortal: React.FC<{
+  children: React.ReactNode;
+  triggerRef: React.RefObject<HTMLElement>;
+  isOpen: boolean;
+}> = ({ children, triggerRef, isOpen }) => {
+  const [styles, setStyles] = useState<React.CSSProperties>({});
+
+  useEffect(() => {
+    if (triggerRef.current && isOpen) {
+      const rect = triggerRef.current.getBoundingClientRect();
+      setStyles({
+        position: 'absolute',
+        top: rect.bottom + window.scrollY,
+        left: rect.left + window.scrollX,
+        zIndex: 9999,
+      });
+    }
+  }, [isOpen, triggerRef]);
+
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(<div style={styles}>{children}</div>, document.body);
+};
+
+const handleUserAction = async (
+  endpoint: string,
+  reservation_id: string,
+  successMessage: string,
+  errorMessage: string,
+  toast: (options: { title: string; description: string }) => void
+) => {
+  try {
+    const response = await fetch(endpoint, {
+      method: 'PATCH',
+      body: JSON.stringify({ reservation_id }),
+    });
+
+    if (!response.ok) throw new Error(errorMessage);
+
+    toast({ title: 'Success', description: successMessage });
+  } catch (error) {
+    console.error(error);
+    toast({ title: 'Error', description: errorMessage });
+  }
+};
+
+const manageUsers = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [openDropdownId, setOpenDropdownId] = useState<string | null>(null);
   const [hasSearched, setHasSearched] = useState(false);
   const [showEmptyState, setShowEmptyState] = useState(true);
   const [filteredUsers, setFilteredUsers] = useState<User[]>();
@@ -31,18 +76,26 @@ export default function Users() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
+  const [openDropdownId, setOpenDropdownId] = useState<string | null>(null);
+  const dropdownRefs = useRef<Record<string, HTMLButtonElement | null>>({});
+  const { toast } = useToast();
+
+  useEffect(() => {
+    const handleClickOutside = () => {
+      if (openDropdownId) setOpenDropdownId(null);
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [openDropdownId]);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         setLoading(true);
-        const response = await fetch('/api/usersList');
+        const response = await fetch('/api/usersList', { method: 'GET' });
+        if (!response.ok) throw new Error('Failed to fetch reservations');
         const data = await response.json();
-
-        if (!response.ok) {
-          throw new Error(data.message || 'Failed to fetch users');
-        }
-
         const mappedUsers = data.map((user: any) => ({
           auth_id: user.auth_id,
           number: user.student_num || user.instructor_id || 'N/A',
@@ -51,9 +104,8 @@ export default function Users() {
           email: user.email,
           phone: user.phone,
           role: user.role,
-          department: user.dept,
+          dept: user.dept,
         }));
-
         setUsers(mappedUsers);
         setFilteredUsers(mappedUsers);
         setShowEmptyState(mappedUsers.length === 0);
@@ -80,15 +132,6 @@ export default function Users() {
     setShowEmptyState(filtered.length === 0);
   };
 
-  const handleClickOutside = useCallback(() => {
-    if (openDropdownId) setOpenDropdownId(null);
-  }, [openDropdownId]);
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
-  }, [handleClickOutside]);
-
   const handleClearSearch = () => {
     setFirstName('');
     setLastName('');
@@ -97,26 +140,14 @@ export default function Users() {
     setShowEmptyState(false);
   };
 
-  const handleDelete = async (authId: string) => {
-    console.log('Deleting user with ID:', authId);
-    try {
-      const response = await fetch(`/api/user/${authId}`, {
-        method: 'DELETE',
-      });
-      console.log('API Call:', `/api/user/${authId}`);
-
-      if (!response.ok) {
-        const data = await response.json();
-        throw new Error(data.error || 'Failed to delete user.');
-      }
-
-      setUsers(prevUsers => prevUsers.filter(user => user.auth_id !== authId));
-      setFilteredUsers(prevUsers => prevUsers?.filter(user => user.auth_id !== authId));
-    } catch (err: any) {
-      console.error('Error deleting user:', err.message);
-      alert(err.message || 'Failed to delete user.');
+  useEffect(() => {
+    const storedToast = localStorage.getItem('user-toast');
+    if (storedToast) {
+      const { title, description } = JSON.parse(storedToast);
+      toast({ title, description });
+      localStorage.removeItem('user-toast');
     }
-  };
+  }, [toast]);
 
   return (
     <div className="bg-white p-6 md:p-10 rounded-3xl drop-shadow-[0_-4px_10px_rgba(0,0,0,0.1)] mx-4 md:mx-20 mt-1 mb-10 font-roboto">
@@ -163,8 +194,9 @@ export default function Users() {
           <span className="hidden md:block">Search</span>
         </button>
       </div>
-
-      <h3 className="text-xl font-semibold font-raleway mb-4 mt-10">Users</h3>
+      <div className="flex flex-row justify-between items-center mb-4">
+        <h2 className="text-lg md:text-2xl font-bold font-raleway">Users</h2>
+      </div>
       <div className="overflow-x-auto">
         <table className="w-full border-collapse shadow-sm rounded-lg overflow-hidden text-sm md:text-base">
           <thead>
@@ -182,68 +214,116 @@ export default function Users() {
               <th className="px-2 md:px-4 py-2 w-[10%] min-w-[80px] rounded-tr-lg">Actions</th>
             </tr>
           </thead>
-          <tbody>
-            {hasSearched && showEmptyState && (
+          <tbody className="bg-white text-center">
+            {users.length > 0 ? (
+              users.map(user => {
+                const ref = (el: HTMLButtonElement | null) => {
+                  dropdownRefs.current[user.auth_id] = el;
+                };
+                return (
+                  <tr key={user.auth_id} className="border-t last:border-b">
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 font-roboto">
+                      {user.number}
+                    </td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
+                      {user.firstName}
+                    </td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 font-roboto">
+                      {user.lastName}
+                    </td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 font-roboto">
+                      {user.email}
+                    </td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 font-roboto">
+                      {user.phone}
+                    </td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 font-roboto">{user.role}</td>
+                    <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
+                      {user.dept}
+                    </td>
+                    <td className="px-0 md:px-0 py-5 relative text-center">
+                      <button
+                        ref={ref}
+                        className="text-gray-500 px-2 py-1 rounded-md"
+                        onClick={e => {
+                          e.stopPropagation();
+                          setOpenDropdownId(openDropdownId === user.auth_id ? null : user.auth_id);
+                        }}
+                      >
+                        <RxDotsHorizontal size={20} />
+                      </button>
+                      <DropdownPortal
+                        isOpen={openDropdownId === user.auth_id}
+                        triggerRef={{
+                          current: dropdownRefs.current[user.auth_id] ?? document.body,
+                        }}
+                      >
+                        <div className="py-2 bg-white rounded-md shadow-xl border border-gray-200 w-40">
+                          <button
+                            className="flex items-center w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                            onClick={async e => {
+                              e.stopPropagation();
+                              router.push(`/admin/edit-profile?user_ID=${user.auth_id}`);
+                              setOpenDropdownId(null);
+                            }}
+                          >
+                            <RxPencil1 size={18} className="mr-2 text-green-500" />
+                            Update
+                          </button>
+                          <button
+                            className="flex items-center w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                            onClick={async e => {
+                              e.stopPropagation();
+                              await handleUserAction(
+                                '/api/user/[id]',
+                                user.auth_id,
+                                'Deleted the user!',
+                                'Failed to delete the user',
+                                toast
+                              );
+                              setOpenDropdownId(null);
+                              localStorage.setItem(
+                                'user-toast',
+                                JSON.stringify({
+                                  title: 'Success',
+                                  description: 'Deleted the reservation!',
+                                })
+                              );
+                              window.location.reload();
+                            }}
+                          >
+                            <RxTrash size={18} className="mr-2 text-red-500" />
+                            Delete
+                          </button>
+                          <button
+                            className="flex items-center w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                            onClick={async e => {
+                              e.stopPropagation();
+                              router.push(`/admin/set-password?user_ID=${user.auth_id}`);
+                              setOpenDropdownId(null);
+                            }}
+                          >
+                            <RxLockOpen1 size={18} className="mr-2 text-gray-500" />
+                            Change password
+                          </button>
+                        </div>
+                      </DropdownPortal>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
               <tr>
-                <td colSpan={9} className="py-10">
-                  <div className="flex flex-col items-center justify-center text-center mt-16 text-gray-500">
-                    <List className="w-12 h-12 mb-4" />
-                    <p className="font-medium text-base text-gray-700">No users searched yet</p>
-                    <p className="text-sm mt-1">
-                      Input the first or last name of the user then click &quot;Search&quot; to find
-                      the user.
-                    </p>
-                  </div>
+                <td colSpan={7} className="text-center px-3 md:px-5 py-3 text-gray-400">
+                  No Users Found
                 </td>
               </tr>
             )}
-            {filteredUsers?.map(user => (
-              <tr key={user.auth_id || user.email} className="border-t last:border-b">
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.number}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.firstName}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.lastName}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.email}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.phone}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.role}
-                </td>
-                <td className="px-3 md:px-5 py-3 hover:bg-gray-100 text-center font-roboto">
-                  {user.department}
-                </td>
-                <td className="px-0 md:px-0 py-5 relative text-center">
-                  <button
-                    className="text-gray-500 px-2 py-1 rounded-md"
-                    onClick={e => {
-                      e.stopPropagation();
-                      setOpenDropdownId(openDropdownId === user.auth_id ? null : user.auth_id);
-                    }}
-                  >
-                    <RxDotsHorizontal size={20} />
-                  </button>
-
-                  {openDropdownId === user.auth_id && (
-                    <UserActionsDropdown
-                      authId={user.auth_id}
-                      onDelete={handleDelete}
-                      onClose={() => setOpenDropdownId(null)}
-                    />
-                  )}
-                </td>
-              </tr>
-            ))}
           </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
+
+export default manageUsers;
