d5e4ba660c88bacc83e94e14ba651795ec1a9624
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-04-26 14:35:43 +0800

    chore/improve schedules ui

--- src/components/schedule/manageSchedules.tsx
+++ src/components/schedule/manageSchedules.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { RxClipboard } from 'react-icons/rx';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
