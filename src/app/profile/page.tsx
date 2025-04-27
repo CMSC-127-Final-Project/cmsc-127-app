c587c2d2e4a7c1748433324a7b22692a7ac37a3f
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-04-27 16:49:28 +0800

    chore/adjusted paddings

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -20,7 +20,7 @@ export default async function HomePage() {
   return (
     <>
       <Navbar user_id={user_id} />
-      <main className="container mx-auto py-6 px-4 md:px-6 flex flex-col md:flex-row gap-6">
+      <main className="container mx-auto py-6 px-4 md:px-6 flex flex-col md:flex-row gap-6 md:mt-10 lg:mt-0">
         <div className="flex flex-col w-full md:w-3/4">
           <Header user_id={user_id} />
           <Settings user_id={user_id} />
