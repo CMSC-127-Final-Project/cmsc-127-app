d224b2645b9ad781995077ed4aa8750e8dbad7d6
Gilean Cyrus Alanza <151953248+Gilbrad@users.noreply.github.com>
2025-04-27 17:31:01 +0800

    feat: setup posthog and sentry (#130)
    * feat: setup posthog and sentry
    
    * chore: auto-fix ESLint & Prettier issues
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://cbb6f82151c5ce11aeff9ce0da9093cd@o4509224230125568.ingest.us.sentry.io/4509224230846464',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
