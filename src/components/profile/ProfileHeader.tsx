954af9dd88a088cf9bea1b0414011eb269ac0f04
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-04-28 01:26:32 +0800

    chore/added loading state for buttons

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,35 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import { LogOut } from 'lucide-react';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function ProfileManagement({ user_id }: { user_id: string }) {
-  const router = useRouter();
-  const [isSigningOut, setIsSigningOut] = useState(false);
-  const handleSignOut = async () => {
-    setIsSigningOut(true);
-    try {
-      const res = await fetch('/api/auth/signout', { method: 'GET' });
-      const data = await res.json();
-
-      if (res.ok) {
-        router.push('/login');
-      } else {
-        console.error(data.error);
-        alert('Sign-out failed. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error during sign-out:', error);
-      alert('An error occurred. Please try again.');
-    } finally {
-      setIsSigningOut(false);
-    }
-  };
-
   const [username, setUsername] = useState();
   useEffect(() => {
     const loadNickname = async () => {
