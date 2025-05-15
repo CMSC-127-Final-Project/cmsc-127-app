7b742205360a8792aca128a48f805a3819f3702d
Cherlie <cipalarpalar@up.edu.ph>
2025-05-15 16:42:45 +0800

    feat: add PWAOptions interface to types.ts

--- src/types/next-pwa.d.ts
+++ src/types/next-pwa.d.ts
@@ -1,27 +1,6 @@
 declare module 'next-pwa' {
   import { NextConfig } from 'next';
-  interface PWAOptions {
-    dest: string;
-    register?: boolean;
-    skipWaiting?: boolean;
-    disable?: boolean;
-    runtimeCaching?: Array<{
-      urlPattern: RegExp | string;
-      handler: string;
-      options?: {
-        cacheName?: string;
-        expiration?: {
-          maxEntries?: number;
-          maxAgeSeconds?: number;
-        };
-        cacheableResponse?: {
-          statuses?: number[];
-          headers?: Record<string, string>;
-        };
-      };
-    }>;
-    buildExcludes?: string[];
-  }
+  import { PWAOptions } from '../utils/types';
 
   const withPWA: (options: PWAOptions) => (nextConfig: NextConfig) => NextConfig;
   export default withPWA;
